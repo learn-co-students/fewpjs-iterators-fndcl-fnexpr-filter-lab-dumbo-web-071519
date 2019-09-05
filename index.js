c5dc85065cf53fcf93f7f541c4e9b5113d5b911f
Janet Mendez <janetmendez.03@gmail.com>
2019-09-05 17:18:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Write a Function To Match object Values To a Provided string
-// Write matchName - This function takes an array of drivers and a string as arguments.In this function, each element of the drivers array is a JavaScript object that has a property of name.The function should return each element whose name property matches the provided string argument.
-
 function findMatching(arr, str) {
     return arr.filter((driver) => {return driver.toLowerCase() == str.toLowerCase()})
 }
