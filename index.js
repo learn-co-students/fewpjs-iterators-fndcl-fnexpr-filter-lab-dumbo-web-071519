e0b17219b80fad1c774aee5ef51f637fe941a5ba
Janet Mendez <janetmendez.03@gmail.com>
2019-09-05 17:16:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// Write a Function To Match object Values To a Provided string
+// Write matchName - This function takes an array of drivers and a string as arguments.In this function, each element of the drivers array is a JavaScript object that has a property of name.The function should return each element whose name property matches the provided string argument.
+
+function findMatching(arr, str) {
+    return arr.filter((driver) => {return driver.toLowerCase() == str.toLowerCase()})
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter((driver) => {
+        return driver.substring(0, str.length) == str
+    })
+}
+
+function matchName(arr, str) {
+    return arr.filter((driver) => { return driver.name === str })
+}
